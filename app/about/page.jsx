f1d6e9399094a400a749740fb4ba17abc3749c4b
Maharun Nessa Meem <maharunnessameem86@gmail.com>
2023-10-08 12:28:43 +0600

    initial setup

---
+++ app/about/page.jsx
@@ -0,0 +1,12 @@
+
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1 className="text-4xl">About Page</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae officiis, omnis quia in molestias similique voluptate magni temporibus quaerat voluptas saepe iste quam quisquam voluptatem doloribus. Exercitationem eligendi similique earum.</p>
+        </div>
+    );
+};
+
+export default AboutPage;
